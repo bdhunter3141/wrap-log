7b84200d3df8303d27bc13992a675eb3fb13e4d6
Bryan Hunter <bdhunter3141@gmail.com>
2017-09-07 23:31:57 +0000

    Completed function and working properly

--- wrap-log.js
+++ wrap-log.js
@@ -1,5 +1,14 @@
 var wrapLog = function (callback, name) {
   /* your code here */
+  return function(x, y, z) {
+    var message = name
+    var argumentList = []
+    for (number in arguments) {
+      argumentList.push(arguments[number])
+    }
+    message += "(" + argumentList + ")" + " => " + callback(x, y, z)
+    console.log(message)
+  }
 };
 
 var area = function (x, y) {
@@ -7,6 +16,7 @@ var area = function (x, y) {
 };
 var logArea = wrapLog(area, "area");
 
+wrapLog(area, "bla")
 logArea(5, 3); // area(5, 3) => 15
 logArea(3, 2); // area(3, 2) => 6
 
@@ -17,3 +27,4 @@ var logVolume = wrapLog(volume, "volume");
 
 logVolume(5, 3, 2); // volume(5, 3, 2) => 30
 logVolume(3, 2, 4); // volume(3, 2, 4) => 24
+
